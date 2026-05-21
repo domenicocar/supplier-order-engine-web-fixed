50be96adb831b19c8363757f0e45370bea6f8361
DomenicoC <domenico.carella@finconsgroup.com>
2026-05-21 13:55:54 +0200

    change be

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://supplier-order-engine.onrender.com',
+  apiBaseUrl: 'https://supplier-order-engine-production.up.railway.app',
   supabaseUrl: 'https://ptdgidxdkdsruchxhtab.supabase.co',
   supabaseAnonKey:
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB0ZGdpZHhka2RzcnVjaHhodGFiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4NzM0NDQsImV4cCI6MjA5NDQ0OTQ0NH0.52ZfVhUFrR7Y67NJI0cMPVBgYjoZcAUoT2QAdTqC9OY'
