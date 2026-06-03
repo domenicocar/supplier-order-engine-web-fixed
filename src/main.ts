d9f215580627611e1ef5533b13c1ee7ae7b77120
DomenicoC <domenico.carella@finconsgroup.com>
2026-06-03 17:30:01 +0200

    vercel analytics

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
+import { inject } from '@vercel/analytics';
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
+import { environment } from './environments/environment';
+
+inject({
+  mode: environment.production ? 'production' : 'development'
+});
 
 bootstrapApplication(AppComponent, appConfig).catch((err: unknown) => {
   console.error(err);
